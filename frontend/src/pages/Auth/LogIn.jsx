846d46cdbec24700d800793a179ed62efa7d2412
insu1170 <insu1170@naver.com>
2024-08-28 12:56:45 +0900

    FEAT: Outlet 사용 Login페이지 Sign페이지 띄우기

---
+++ frontend/src/pages/Auth/LogIn.jsx
@@ -0,0 +1,48 @@
+import {Link, Outlet} from "react-router-dom";
+import styled from "styled-components"
+import {Center} from "../../styles/stylePart";
+
+export const LogIn = () => {
+
+
+    return (<Container>
+        <Center style={{height: '30%'}}>
+            <Link to="/login">
+                <ChangePageBtn left>로그인</ChangePageBtn>
+            </Link>
+            <Link to="/signup">
+                <ChangePageBtn>회원가입</ChangePageBtn>
+            </Link>
+        </Center>
+        <Center>
+            <OutletDiv>
+                <Outlet/>
+            </OutletDiv>
+        </Center>
+    </Container>);
+}
+//---- Style Components ----//
+const Container = styled.div`
+  background-color: #888888;
+  width: 100%;
+  height: 100vh;
+`
+const OutletDiv = styled.div`
+  background-color: aliceblue;
+  padding: 20px;
+  border-radius: 0 0 20px 20px;
+`
+const ChangePageBtn = styled.button`
+  width: 240px;
+  height: 40px;
+  border: none;
+  border-radius: ${(props) => props.left ? '20px 0 0 0' : '0 20px 0 0'};
+  border-right: ${(props) => props.left ? '1px solid black' : 'none'};
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: bolder;
+
+  &:hover {
+    background-color: #dee7f1;
+  }
+`;
