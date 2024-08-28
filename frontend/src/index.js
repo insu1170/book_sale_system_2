8f3e4f61fc21ecfb6316ce403ce631e85390c1ec
insu1170 <insu1170@naver.com>
2024-08-28 12:53:50 +0900

    FEAT: Outlet 기능 사용을 위한 louter 설정 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { LoginPage } from "./pages/Auth/LoginPage";
+import { SignUpPage } from "./pages/Auth/SignUpPage";
+import { LogIn } from "./pages/Auth/LogIn";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LogIn />,
+        children: [
+            {
+                path: "login",
+                element: <LoginPage />
+            },
+            {
+                path: "signup",
+                element: <SignUpPage />
+            }
+        ]
+    },
+    {
+        path: "/Main",
+        element: <App />,
+        children: [
+            {
+                path: "login",
+                element: <LoginPage />
+            },
+            {
+                path: "signup",
+                element: <SignUpPage />
+            }
+        ]
+    }
+]);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode><RouterProvider router={router} /></React.StrictMode>);
