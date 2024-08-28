5f4838db8e6c546dabf367aba05786024a8f511b
insu1170 <insu1170@naver.com>
2024-08-28 22:20:35 +0900

    FEAT: 카드번호 확인을 위한 중복체크 기능 DoubleCheck 추가

--- frontend/src/hooks/useInput.js
+++ frontend/src/hooks/useInput.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import selectData from "../utils/sql/selectData";
 
 //** 첫 번째 매개변수를 input의 value에 넣고 onChange에 두 번째꺼 */
 export function useInput(value) {
@@ -7,6 +8,18 @@ export function useInput(value) {
         setInputValue(e.target.value)
     }
 
+    const DoubleCheck = async (e) => {
+        setInputValue(e.target.value)
+        if (e.target.value.length >= 16) {
+            const check = await selectData('card', '/select', ['cardNum'], ['cardNum'], [e.target.value])
+            console.log('check:', check);
+            if (check.success) {
+                alert(`카드번호 ${e.target.value}는 사용할 수 없습니다 `)
+            }else{
+                alert('가능')
+            }
+        }
+    }
 
-    return [inputValue,changeHandle];
+    return [inputValue, changeHandle, DoubleCheck];
 }
