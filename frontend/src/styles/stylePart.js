ae44774124edff170c0238e7d2f3533cef708dca
insu1170 <insu1170@naver.com>
2024-08-29 15:05:03 +0900

    REFACTOR : addInputs에 사용할 스타일 컴포넌트 추가

--- frontend/src/styles/stylePart.js
+++ frontend/src/styles/stylePart.js
@@ -12,4 +12,53 @@ const AddContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
-export {Center,AddContainer}
+
+const AddLabel = styled.label`
+  font-size: 20px;
+  margin-bottom: 5px;
+  font-weight: 600;
+`;
+
+const AddInputDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 10px 0;
+  width: max(500px, 50vh);
+  height: 90px;
+`;
+
+const AddInput = styled.input`
+  width: 100%;
+  height: 50px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+  box-sizing: border-box;
+  
+  &:focus {
+    outline: none;
+    border-color: #9191ff;
+  }
+`;
+
+const FlexDiv = styled.div`
+display: flex;
+`
+const StyledButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  background-color: #878d8d;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #5f5fd4;
+  }
+`;
+
+export {Center,AddContainer,AddLabel,AddInput,AddInputDiv,FlexDiv,StyledButton}
