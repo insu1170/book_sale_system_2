fa3cd7f66fec109c7bd6b22ad59c4ce0a4c1ab8f
insu1170 <insu1170@naver.com>
2024-09-05 01:32:24 +0900

    FEAT: 서버 POST 요청 및 값 반환 기능 추가

--- frontend/src/apis/api/user.js
+++ frontend/src/apis/api/user.js
@@ -1,9 +1,21 @@
 import {instance} from "../instance/instance";
 
-export const userPostApi = async (endPoint, data) => {
+export const userPostApi = async (endPoint, ...data) => {
+    console.log(data, 'fd', ...data)
     try {
-        return await instance.post(`/user/${endPoint}`, data)
+        console.log('요청이 왔어요');
+        const result = await instance.post(`/user/${endPoint}`, data)
+        if (result.status === 200) {
+            if (result.data.length === 0) {
+                return {data: result.data, success: false}
+            }
+            return {data: result.data, success: true}
+
+        } else {
+            return alert('오류가 발생했습니다')
+        }
     } catch (err) {
+        alert('실패')
         console.log(err)
     }
 }
