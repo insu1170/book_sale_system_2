902b8d4d9af0d061e9b2935950e91a12b0a02d6f
insu1170 <insu1170@naver.com>
2024-09-04 12:43:59 +0900

    REFACTOR: 인자를 구조 분해 할당으로 선언

--- frontend/src/utils/sql/insertData.js
+++ frontend/src/utils/sql/insertData.js
@@ -4,9 +4,9 @@ import port from "../port";
 
 /** 테이블 이름, 서버 url, key값, value값 넣으면 맞춰서 insert 문 실행*/
 async function insertData(tableName, url, ...query) {
-
-    const columns = query[0]; //key 값
-    const values = query[1];  // value 값
+    // const columns = query[0]; //key 값
+    // const values = query[1];  // value 값
+    const [columns, values] = query
     const columnString = columns.join(', '); // key 값 배열을 문자열로 + 배열 값마다 , 를 넣는 과정
     const queryMark = values.map(() => '?').join(', '); // 값 개수만큼 ? 문자열 생성및 위와 같이 ,를 넣는 과정
 
@@ -17,8 +17,7 @@ async function insertData(tableName, url, ...query) {
             sql: sqlQuery, value: values
         }, axiosPost(true));
         // 응답 처리
-        const data = response.data;
-
+        const {data} = response;
         console.log(data)
         if (data.success) {
             return data
--- frontend/src/utils/sql/selectData.js
+++ frontend/src/utils/sql/selectData.js
@@ -5,17 +5,18 @@ import port from "../port";
 /** 테이블 이름 ,URL, 얻을 value 값 이름, 비교할 값이름, 값 */
 async function selectData(tableName, url, ...query) {
     console.log(tableName, url, ...query);
-    const queryData = query[0];
-    const filterKey = query[1]
-    const filterVal = query[2]
-const key = filterKey.map((data)=>`${data} = ?`).join(' AND ')
-console.log(`SELECT ${queryData} FROM \`${tableName}\` WHERE ${key}`,',',filterVal)
+    /*  const queryData = query[0];
+      const filterKey = query[1]
+      const filterVal = query[2]*/
+    const [queryData, filterKey, filterVal] = query
+    const key = filterKey.map((data) => `${data} = ?`).join(' AND ')
+    console.log(`SELECT ${queryData} FROM \`${tableName}\` WHERE ${key}`, ',', filterVal)
     const sql = `SELECT ${queryData} FROM \`${tableName}\` WHERE ${key}`;
-    console.log(sql, filterVal,url)
+    console.log(sql, filterVal, url)
     const data = await axios.post(`${port}${url}`, {
-        sql: sql, filterVal: filterVal
+        sql, filterVal
     }, axiosPost(true))
-    console.log('selectData:',data.data)
+    console.log('selectData:', data.data)
     return (data.data)
 }
 
