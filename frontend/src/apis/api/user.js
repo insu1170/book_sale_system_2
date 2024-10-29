cc14a05bcf5a4579997960a9348dc355c94ea85d
insu1170 <insu1170@naver.com>
2024-10-30 04:01:33 +0900

    FEAT  : Get 요청 작업 추가

--- frontend/src/apis/api/user.js
+++ frontend/src/apis/api/user.js
@@ -19,3 +19,23 @@ export const userPostApi = async (endPoint, ...data) => {
         console.log(err)
     }
 }
+
+export const userGetApi = async (endPoint, ...data) => {
+
+    try {
+        const result = await instance.get(`/user/${endPoint}`, {params: data});
+        if (result.status === 200) {
+            if (result.data.length === 0) {
+                return {data: result.data, success: false}
+            }
+            return {data: result.data, success: true}
+        } else {
+            return alert('오류가 발생했습니다')
+        }
+
+    } catch (err) {
+        alert('작업 실패');
+        console.log(err);
+    }
+
+}
