a959ef4bbe049fa83fb4040fdc0586658ba77511
insu1170 <insu1170@naver.com>
2024-11-02 21:59:15 +0900

    FEAT  : 책 추가 기능 추가

--- frontend/src/pages/AddPages/AddBook.jsx
+++ frontend/src/pages/AddPages/AddBook.jsx
@@ -1,24 +1,31 @@
 import {AddContainer, StyledButton} from "../../styles/stylePart";
 import {AddInputs} from "../../components/Part";
 import {useInput} from "../../hooks/useInput";
+import {userPostApi} from "../../apis/api/user";
 
 export const AddBook = () => {
-    const [inputValue, setInputHandle,doubleCheckHandle] = useInput({
-        bookId: "", name: '', price: "", quantity: '', imgUrl: '' , doubleCheck:''
+    const [inputValue, setInputHandle, doubleCheckHandle] = useInput({
+        bookId: "", name: '', price: "", quantity: '', imgUrl: '', doubleCheck: ''
     })
+    const addBookInfo = async () => { // insert
+        const sends = await userPostApi('insert', `book`, [`bookId`, `name`, `price`, `quantity`, `bookUrl`], [inputValue.bookId, inputValue.name, inputValue.price, inputValue.quantity, inputValue.imgUrl])
+        console.log(sends)
+    }
 
-    return (<AddContainer>
-        <h1>도서 추가 창</h1>
-        <AddInputs label='도서번호' name='bookId' onChange={doubleCheckHandle} placeholder='사용 할 제품번호'
-                   value={inputValue.bookId} text={inputValue.doubleCheck}></AddInputs>
-        <AddInputs label='책 제목' name='name' onChange={setInputHandle} placeholder='책 제목'
-                   value={inputValue.name}></AddInputs>
-        <AddInputs label='개당 가격' name='price' onChange={setInputHandle} placeholder='제품 가격 개당'
-                   value={inputValue.price}></AddInputs>
-        <AddInputs label='등록 수량' name='quantity' onChange={setInputHandle} placeholder='등록 할 수량'
-                   value={inputValue.quantity}></AddInputs>
-        <AddInputs label='제품 이미지' name='imgUrl' onChange={setInputHandle} placeholder='이미지 URL 입력 없을 경우 빈칸 제출'
-                   value={inputValue.imgUrl} ></AddInputs>
-        <StyledButton>등록하기</StyledButton>
-    </AddContainer>)
+    return (
+        // onsubit 으로
+        <AddContainer>
+            <h1>도서 추가 창</h1>
+            <AddInputs label='도서번호' name='bookId' onChange={doubleCheckHandle} placeholder='사용 할 제품번호'
+                       value={inputValue.bookId} text={inputValue.doubleCheck}></AddInputs>
+            <AddInputs label='책 제목' name='name' onChange={setInputHandle} placeholder='책 제목'
+                       value={inputValue.name}></AddInputs>
+            <AddInputs label='개당 가격' name='price' onChange={setInputHandle} placeholder='제품 가격 개당'
+                       value={inputValue.price}></AddInputs>
+            <AddInputs label='등록 수량' name='quantity' onChange={setInputHandle} placeholder='등록 할 수량'
+                       value={inputValue.quantity}></AddInputs>
+            <AddInputs label='제품 이미지' name='imgUrl' onChange={setInputHandle} placeholder='이미지 URL 입력 없을 경우 빈칸 제출'
+                       value={inputValue.imgUrl}></AddInputs>
+            <StyledButton onClick={addBookInfo}>등록하기</StyledButton>
+        </AddContainer>)
 }
