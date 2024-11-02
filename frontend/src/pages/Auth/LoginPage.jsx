e2a674687ca0e399492193e84e762e404c8368d5
insu1170 <insu1170@naver.com>
2024-11-02 22:08:48 +0900

    FEAT: 로그인시 cookie에 값 넣는 기능 추가

--- frontend/src/pages/Auth/LoginPage.jsx
+++ frontend/src/pages/Auth/LoginPage.jsx
@@ -1,43 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 import {useInput} from "../../hooks/useInput";
-import {useFormSubmit} from "../../hooks/useFormSubmit";
-import {AddContainer,AddInput} from "../../styles/stylePart";
+import {LoginContainer} from "../../styles/stylePart";
 import {AddInputs} from "../../components/Part";
-
+import {userPostApi} from "../../apis/api/user";
+import useNavigate from "../../utils/useNavigate";
+import Cookies from "js-cookie";
 
 export const LoginPage = () => {
 
-    const [inputValue,handleChange]=useInput({
-        idValue:'', pwValue:''
+    const [inputValue, handleChange] = useInput({
+        idValue: '', pwValue: ''
     })
-    const {handleSubmit} = useFormSubmit('login');
-
+    const navigateToHome = useNavigate('/main/main')
     const loginHandle = async (e) => {
+
         e.preventDefault();
-        await handleSubmit({id: inputValue.idValue, password: inputValue.pwValue});
+        const result = await userPostApi('login', ['*'], ['userId', 'passWord'], [inputValue.idValue, inputValue.pwValue])
+        console.log('요청 결과:', result,); // 요청 결과 확인
+        console.log(result.success)
+        if (result.success && result.data[0]) {
+            alert('있음')
+            Cookies.set('id', result.data[1][0].userId, {expires: 1}); // 1일 후 만료
+            navigateToHome()
+        } else {
+            alert('없음')
+        }
 
     };
 
-
     return (<div>
-            <AddContainer>
-                <h2>Login</h2>
-                <form onSubmit={loginHandle}>
-                    <AddInputs label='아이디:' placeholder='아이디 입력' value={inputValue.idValue} name='idValue' onChange={handleChange}/>
-                    <AddInputs  label='비밀번호:' placeholder='비밀번호 입력' value={inputValue.pwValue} name='pwValue' onChange={handleChange}/>
-                    <Button type="submit">로그인</Button>
-                </form>
-            </AddContainer>
-        </div>);
+        <LoginContainer>
+            <h2>Login</h2>
+            <div>
+                <AddInputs label='아이디:' placeholder='아이디 입력' value={inputValue.idValue} name='idValue'
+                           onChange={handleChange}/>
+                <AddInputs label='비밀번호:' placeholder='비밀번호 입력' value={inputValue.pwValue} name='pwValue'
+                           onChange={handleChange}/>
+                <Button type='submit'
+                        onClick={loginHandle}>로그인</Button>
+            </div>
+        </LoginContainer>
+    </div>);
 };
 
 
 // Styled components
-
-
-
-
 
 const Button = styled.button`
   background-color: #007bff;
